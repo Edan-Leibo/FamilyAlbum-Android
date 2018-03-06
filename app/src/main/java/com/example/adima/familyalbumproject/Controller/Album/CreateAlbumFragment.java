38bca38353468a7973ea24e75b12496785191b46
Edan Leibovitz <leibo.edan@gmail.com>
2018-03-06 13:11:55 +0200

    Date Picker was added in create new album

--- app/src/main/java/com/example/adima/familyalbumproject/Controller/Album/CreateAlbumFragment.java
+++ app/src/main/java/com/example/adima/familyalbumproject/Controller/Album/CreateAlbumFragment.java
@@ -1,19 +1,31 @@
 package com.example.adima.familyalbumproject.Controller.Album;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ProgressBar;
 
+import com.example.adima.familyalbumproject.Album.Model.Album;
 import com.example.adima.familyalbumproject.Controller.MainActivity;
 import com.example.adima.familyalbumproject.R;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+import Model.Model;
 
 
 public class CreateAlbumFragment extends Fragment {
+    private final static String FAMILY_SERIAL = "FAMILY_SERIAL";
+    private EditText albumDate;
+    private Calendar myCalendar;
 
     @Nullable
     @Override
@@ -24,19 +36,19 @@ public class CreateAlbumFragment extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-/*
-        String serialNumber;
+        final ProgressBar progressBar = view.findViewById(R.id.createNewAlbumProgressBar);
+        final EditText albumName = (EditText) view.findViewById(R.id.createAlbum_album_name);
+        albumDate= (EditText) view.findViewById(R.id.createAlbum_album_date);
+        final EditText albumLocation= (EditText) view.findViewById(R.id.createAlbum_album_location);
+
+
+        final String familySerial = Model.instance().getFamilySerialFromSharedPrefrences("familyInfo",FAMILY_SERIAL);
+
         Album album = new Album();
-        album.setSerialNumber();
-        album.setDate();
-        album.setName();
-        album.setLocation();
-        Model.instance().addAlbumToFirebase(album,serialNumber);
-*/
+
         view.findViewById(R.id.btn_cancel).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                ProgressBar progressBar = view.findViewById(R.id.createNewAlbumProgressBar);
                 progressBar.setVisibility(View.VISIBLE);
                 ((MainActivity) getActivity()).showAlbumsFragment();
             }
@@ -45,10 +57,50 @@ public class CreateAlbumFragment extends Fragment {
         view.findViewById(R.id.btn_create_album).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Album album = new Album();
+                album.setSerialNumber(familySerial);
+                album.setName(albumName.getText().toString());
+                album.setDate(albumDate.getText().toString());
+                album.setLocation(albumLocation.getText().toString());
+                Model.instance().addAlbumToFirebase(album,familySerial);
                 ((MainActivity) getActivity()).showAlbumFragment();
             }
         });
 
+        progressBar.setVisibility(View.GONE);
 
+        myCalendar = Calendar.getInstance();
+
+        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                // TODO Auto-generated method stub
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabel();
+            }
+
+        };
+
+        albumDate.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                new DatePickerDialog(getActivity(), date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+    }
+
+    private void updateLabel() {
+        String myFormat = "dd/MM/yy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+        albumDate.setText(sdf.format(myCalendar.getTime()));
     }
 }
